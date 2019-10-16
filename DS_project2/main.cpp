3c9af768dd09784f19832e23f21c439e9ffbda41
朱世耘 <oikbyijm@yahoo.com.tw>
2019-10-16 14:29:43 +0800

    Initial Commit

---
+++ DS_project2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DS_project2
+//
+//  Created by 朱世耘 on 2019/10/16.
+//  Copyright © 2019 sy_chu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
