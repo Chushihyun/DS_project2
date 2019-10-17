b8e321ee000426935bec4e4ac6bd8729aefa7e7f
朱世耘 <oikbyijm@yahoo.com.tw>
2019-10-17 23:09:46 +0800

    10.17

--- DS_project2/main.cpp
+++ DS_project2/main.cpp
@@ -7,11 +7,35 @@
 //
 
 #include <iostream>
+#include <cstdlib>
 using namespace std;
+
+//// class:每個位置
+
+class location{
+    public:
+    
+    int x;
+    int y;
+    bool finish;// false = unfinish, true = finish
+    int distance_charge;
+    int distance_now;
+    
+    location(){}
+    location(int a,int b){
+        x=a;
+        y=b;
+        finish=false;
+    }
+    void output(){
+        cout<<"("<<x<<","<<y<<")"<<endl;
+    }
+};
+
+////輸出整張地圖
 
 int row, column, energy_max;
 int** A;
-
 
 void check_matrix(){
     for (int m=row+1; m>=0; m--) {
@@ -23,8 +47,59 @@ void check_matrix(){
     cout<<endl;
 }
 
+//// 輸出所有目標位置ㄝ，A[0]為充電站，A[1]~A[num]為路
 
+location* all;
+int num=0;
 
+void check_location(){
+    cout<<"x y done"<<endl;
+    for (int i=0; i<=num; i++) {
+        cout<<all[i].x<<" "<<all[i].y<<" "<<all[i].finish<<endl;
+    }
+    cout<<endl;
+}
+
+//// all pair shortest path, collect in B[num+1][num+1]
+
+void calculate_distance(){
+    int B[num+1][num+1];
+    for (int i=0; i<=num; i++) {
+        for (int j=0; j<=num; j++) {
+            if (i==j) {
+                B[i][j]=0;
+            }
+            else if ( (abs(all[i].x-all[j].x) + abs(all[i].y-all[j].y)) ==1 ){
+                B[i][j]=1;
+            }
+            else{
+                B[i][j]=2*(row+column);
+            }
+        }
+    }
+////Floyd-Warshall algorithm
+    for (int k=0; k<=num; k++) {
+        for (int i=0; i<=num; i++) {
+            for (int j=0; j<=num; j++) {
+                if (B[i][j]>B[i][k]+B[k][j]) {
+                    B[i][j]=B[i][k]+B[k][j];
+                }
+            }
+        }
+    }
+    
+    
+    for (int i=0; i<=num; i++) {
+        for (int j=0; j<=num; j++) {
+            cout<<B[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    
+    
+}
+
+///// main
 int main() {
     
     freopen("testcase.data", "r", stdin);
@@ -49,13 +124,27 @@ int main() {
             }
         }
     }
-    /////////////////// initial finished
+    /////////////////// input finished  0:可走 1:不可走 2:充電站
     
-    //find charge_point
+    all=new location[row*column+1];
     
-    //collect all unfinish
+    for (int x=1; x<=column; x++) {
+        for (int y=1; y<row; y++) {
+            if (A[x][y]==0) {
+                num++;
+                all[num]=location(x, y);
+            }
+            else if (A[x][y]==2){
+                all[0]=location(x, y);
+                all[0].finish=true;
+            }
+        }
+    }
     
+    check_location();
+
     //calculate all relative distance
+    calculate_distance();
     
     //from the farest
     //do second weighted_farest
